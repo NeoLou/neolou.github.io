cbda8accf48c09356eaabee049391830fdb26835
NeoLou <neolou2000@hotmail.com>
2021-06-20 01:26:13 -0400

    fix scroll to top button not appearing

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+//Scroll to top button
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
 //Rollcage Animation
 
 function myFunction(x) {
@@ -20,29 +41,9 @@ function myFunction(x) {
 }
 }
 
-var x = window.matchMedia("(max-width:600px)")
-myFunction(x) // Call listener function at run time
-x.addEventListener("change", myFunction()) // Attach listener function on state changes  
-
-//Scroll to top button
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+var x = window.matchMedia("(max-width:600px)");
+myFunction(x); // Call listener function at run time
+x.addEventListener("change", myFunction()); // Attach listener function on state changes  
 
 //Contact form
 
